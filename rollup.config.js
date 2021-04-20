0763edc11338bab0995d891ebda984382b29ba78
Umang Galaiya <umanghome@gmail.com>
2021-04-20 13:29:46 +0530

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 
-const production = true || !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
   input: "index.js",
