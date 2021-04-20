e2b3af3752851d2995314c61a30d424bad32085a
Umang Galaiya <umanghome@gmail.com>
2021-04-20 14:44:29 +0530

    Add Other Resources and update footer

--- App.svelte
+++ App.svelte
@@ -247,6 +247,10 @@
       order: 3;
     }
 
+    #other-resources {
+      order: 4;
+    }
+
     .list-split-on-mobile {
       display: flex;
       flex-wrap: wrap;
@@ -342,11 +346,17 @@
         </li>
       </ol>
     </div>
+    <div id="other-resources">
+      <h2>Other Resources</h2>
+      <ul>
+        <li><a href="https://covidfacts.in/" target="_blank" rel="noopener noreferrer">covidfacts.in</li>
+      </ul>
+    </div>
   </div>
 
   <div class="feedback">
-    <a href="https://twitter.com/umanghome/status/1383759182495588355" target="_blank" rel="noopener noreferrer">Got feedback?</a>
-    <br />
-    <a href="https://github.com/umanghome/twitter-search-covid19" target="_blank" rel="noopener noreferrer">Source code</a>
+    <div><a href="https://twitter.com/umanghome/status/1383759182495588355" target="_blank" rel="noopener noreferrer">Got feedback?</a></div>
+    <div><a href="https://github.com/umanghome/twitter-search-covid19" target="_blank" rel="noopener noreferrer">Source code</a></div>
+    <div>Made by <a href="https://twitter.com/umanghome" target="_blank" rel="noopener noreferrer">Umang</a></div>
   </div>
 </main>
