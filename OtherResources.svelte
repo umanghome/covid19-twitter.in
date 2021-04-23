1a669d5dbddc90d25c418d9062861ec072610d65
Viksit Gaur <viksit@users.noreply.github.com>
2021-04-23 13:30:34 -0700

    Add covidfyi.in as a resource
    Add covidfyi.in as a resource

--- OtherResources.svelte
+++ OtherResources.svelte
@@ -2,5 +2,6 @@
   <h2>Other Resources</h2>
   <ul>
     <li><a href="https://covidfacts.in/" target="_blank" rel="noopener noreferrer">covidfacts.in</a></li>
+    <li><a href="https://life.coronasafe.network/" target="_blank" rel="noopener noreferrer">COVIDFYI.in - Verified database of resources</a></li>
   </ul>
 </div>
