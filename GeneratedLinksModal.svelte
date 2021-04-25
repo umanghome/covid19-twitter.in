3421f3d0d33ca44aae3a0df7ca8c524f4f38d3c1
Umang Galaiya <umanghome@gmail.com>
2021-04-25 17:04:33 +0530

    Add demo for ad

--- GeneratedLinksModal.svelte
+++ GeneratedLinksModal.svelte
@@ -5,5 +5,26 @@
   export let links;
 </script>
 
+<style>
+  .ad {
+    background: #777777;
+    color: white;
+    height: 100px;
+    width: 300px;
+    max-width: 100%;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+  }
+
+  p {
+    text-align: center;
+  }
+</style>
+
 <Tips large={false} />
 <GeneratedLinks {links} />
+<p>Our sponsor donated &lt;amount&gt; to &lt;place&gt; to show you this ad</p>
+<div class="ad">Your ad goes here</div>
