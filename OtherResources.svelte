dc84ffc3d5a7a0a1f965fa8c8d833b9f84dc8bd7
Atul Jangra <atuljangra66@gmail.com>
2021-04-24 21:46:24 -0400

    Add description to covidfacts.in
    Let's add a description for this link so that people know what to expect and can help out.

--- OtherResources.svelte
+++ OtherResources.svelte
@@ -1,6 +1,6 @@
 <div>
   <h2>Other Resources</h2>
   <ul>
-    <li><a href="https://covidfacts.in/" target="_blank" rel="noopener noreferrer">covidfacts.in</a></li>
+    <li><a href="https://covidfacts.in/" target="_blank" rel="noopener noreferrer">covidfacts.in</a> Crowdsourced data for COVID resources.</li>
   </ul>
 </div>
