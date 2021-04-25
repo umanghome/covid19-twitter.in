64260a1d4e7ed8c5f3c81334aed562b2b7b6d629
Umang Galaiya <umanghome@gmail.com>
2021-04-25 18:38:21 +0530

    Update ad message

---
+++ Ad.svelte
@@ -0,0 +1,28 @@
+<style>
+  .ad {
+    background: #777777;
+    color: white;
+    height: 100px;
+    width: 350px;
+    max-width: 100%;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    border-radius: 10px;
+    text-decoration: none;
+    padding: 8px;
+  }
+
+  p {
+    text-align: center;
+  }
+</style>
+
+<p>Our sponsor donated &lt;amount&gt; to &lt;place&gt; to show you this ad</p>
+<a href="https://twitter.com/umanghome/status/1386285176255180808" target="_blank" class="ad">
+  This page got 1,88,000 views yesterday.
+  <br />
+  Want to run your ad here? Click me.
+</a>
