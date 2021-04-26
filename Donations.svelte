6d0e3aeaa5569c0235ccdca50757186653387a71
biren25 <biren25.prajapati@gmail.com>
2021-04-27 01:00:38 +0530

    Update Donations.svelte

--- Donations.svelte
+++ Donations.svelte
@@ -3,7 +3,7 @@
   <ul>
     <li><a href="https://hemkuntfoundation.com/donate-now/" target="_blank" rel="noopener noreferrer">Hemkunt Foundation</a> has been helping out with Oxygen Cylinders. 80G donation receipts available.</li>
     <li><a href="https://actgrants.in/donate/" target="_blank" rel="noopener noreferrer">ACT grants</a> has been helping with distribution of oxygen. Run by Indian startup ecosystem. International donations also accepted.</li>
-    <li><a href="https://indiafightscorona.giveindia.org/safeperiod/" target="_blank" rel="noopener noreferrer">Give India</a> Sanitary napkins for women affected by the Pandemic.</li>
+    <li><a href="https://indiafightscorona.giveindia.org/safeperiod/" target="_blank" rel="noopener noreferrer">Give India</a> are providing sanitary napkins for women affected by the Pandemic.</li>
     <li><a href="https://www.ketto.org/fundraiser/FeedingfromfarForCorona" target="_blank" rel="noopener noreferrer">Feeding From Far</a> has been feeding the poor and unemployed who are struggling to feed themselves during the lockdown. Accepts international payments.</li>
     <li><a href="https://www.impactguru.com/fundraiser/oxygen" target="_blank" rel="noopener noreferrer">Swasth</a> along with ACT Grants is procuring oxygen concentrators and channeling them to hospitals in India.</li>
     <li><a href="https://www.feedingindia.org/donate/help-save-my-india?review_id=oxygen" target="_blank" rel="noopener noreferrer">Zomato Feeding India</a> are providing hospitals and patients with oxygen and related supplies for free. Accepts international payments.</li>
