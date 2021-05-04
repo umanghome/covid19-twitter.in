4b6142d734d8e8bd67022d8e0403b7c8e98e7034
Shrinidhi Mahishi <shrinidhimahishi401@gmail.com>
2021-05-04 11:28:06 +0530

    remove numbers
    remove numbers as i was using ul

--- Importantinformation.svelte
+++ Importantinformation.svelte
@@ -14,33 +14,33 @@
 
     <li>1.Ensure the cencentrator has high flow rate.10 L is great</li>
     <li>
-      2.Buy the "90%±3%" concentration output ,not "30-90±%". Very very
+      Buy the "90%±3%" concentration output ,not "30-90±%". Very very
       important.
     </li>
     <li>
-      3.Do NOT buy Dedakj (not for COVID!)&Phillips 3ltr (pretty useless).Go for
+       Do NOT buy Dedakj (not for COVID!)&Phillips 3ltr (pretty useless).Go for
       airsep or phillips 5l -both work
     </li>
-    <li>4.Certification matters.</li>
+    <li> Certification matters.</li>
     <li>
-      6.If you'r using US oxygen concentrator ,they're only 110V.You will need a
+      If you'r using US oxygen concentrator ,they're only 110V.You will need a
       transformer to step it up to 230V.
     </li>
     <li>
-      7.Please watch out for this: sellers cant try to fool you with high prices
+       Please watch out for this: sellers cant try to fool you with high prices
       by highlighting 5ltr,8ltr,10ltr flow rates.THIS IS NOT ENOUGH.Ensure
       output of 90%(±3%) concentration of oxygen .This high percentage really is
       the need of the hour
     </li>
     <li>
-      8.Lastly concentrators that do not fulfil these conditions are less ideal
+      Lastly concentrators that do not fulfil these conditions are less ideal
       for covid cases.But,if there is no other option,other concentrators still
       save lives by providing patients the necessary relief -so please purchase
       them if all else fails
     </li>
     <li>Take care of yourself and your loved ones :)</li>
     <li>
-      9.Final note on prices:this is important; do NOT assume that more
+       Final note on prices:this is important; do NOT assume that more
       expensive means better quality.Many cheap concentrators can be good and
       exprensive once can be rubbish .refer to all the above notes and ensure
       they are met.concentrators usually cost between Rs 40K-90K
